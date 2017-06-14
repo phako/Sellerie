ce2bb8ab487d4d65b877c4d6b7d93a8591a2609c
Jens Georg <mail@jensge.org>
2017-06-14 14:55:59 +0200

    wip: Use GtkApplication

--- src/gtkterm.c
+++ src/gtkterm.c
@@ -36,10 +36,45 @@
 GtSerialPort *serial_port;
 extern struct configuration_port config;
 GtBuffer *buffer;
+extern GtkWidget *Fenetre;
+
+static void
+on_gtk_application_startup (GApplication *app,
+                            gpointer      user_data)
+{
+  GtkBuilder *builder = gtk_builder_new_from_resource ("/org/jensge/GtkTerm/main-window.ui");
+  GMenuModel *menu_model = G_MENU_MODEL (gtk_builder_get_object (builder, "window-menu"));
+  g_critical ("Menu model: %p", menu_model);
+  gtk_application_set_menubar (GTK_APPLICATION (app), menu_model);
+
+  g_object_unref (builder);
+}
+
+static void
+on_gtk_application_activate (GApplication *app,
+                             gpointer      user_data)
+{
+  create_main_window(GTK_APPLICATION (app));
+
+#if 0
+  if (read_command_line(argc, argv) < 0)
+  {
+      exit (EXIT_FAILURE);
+  }
+#endif
+
+  gt_serial_port_config (serial_port, &config);
+
+  add_shortcuts();
+
+  set_view(ASCII_VIEW);
+}
 
 int main(int argc, char *argv[])
 {
   char *config_file;
+  GtkApplication *app = NULL;
+  int status;
 
   buffer = gt_buffer_new ();
   serial_port = gt_serial_port_new ();
@@ -54,26 +89,18 @@ int main(int argc, char *argv[])
   bind_textdomain_codeset(PACKAGE, "UTF-8");
   textdomain(PACKAGE);
 
-  gtk_init(&argc, &argv);
+  app = gtk_application_new ("org.jensge.GtkTerm",
+//                             G_APPLICATION_HANDLES_COMMAND_LINE |
+                             G_APPLICATION_NON_UNIQUE);
 
+  g_signal_connect(app, "activate", G_CALLBACK (on_gtk_application_activate), NULL);
+  g_signal_connect(app, "startup", G_CALLBACK (on_gtk_application_startup), NULL);
 
-  create_main_window();
-
-  if (read_command_line(argc, argv) < 0)
-  {
-      exit (EXIT_FAILURE);
-  }
-
-  gt_serial_port_config (serial_port, &config);
-
-  add_shortcuts();
-
-  set_view(ASCII_VIEW);
-
-  gtk_main();
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
 
   gt_serial_port_close_and_unlock (serial_port);
   g_object_unref (serial_port);
 
-  return EXIT_SUCCESS;
+  return status;
 }
